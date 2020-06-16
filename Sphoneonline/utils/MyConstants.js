f0358cdf0a0ebd881d5044f558ca4a42843a0542
Nguyễn Phạm Hoàng Long <long.187pm13956@vanlanguni.vn>
2020-06-16 22:09:13 +0700

    Up CSS + HTML

---
+++ Sphoneonline/utils/MyConstants.js
@@ -0,0 +1,10 @@
+var MyConstants = {
+  DB_SERVER: 'cluster0-zp9ao.mongodb.net',
+  DB_USER: 'hoanglong',
+  DB_PASS: '15112000',
+  DB_DATABASE: 'shoppingonline',
+  HOSTNAME: 'localhost:3000',
+  EMAIL_USER: 'long.187pm13956@vanlanguni.vn',
+  EMAIL_PASS: 'VLU187pm13956',
+};
+module.exports = MyConstants;
